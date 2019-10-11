55a314b426d292bac56bc1d7a5c02002a2009fee
dulmishanika <56398705+dulmishanika@users.noreply.github.com>
2019-10-11 21:34:01 +0530

    Add files via upload

--- oddeven.java
+++ oddeven.java
@@ -1,33 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package myclass;
 
-/**
- *
- * @author Dulmi
- */import java.util.Scanner;
+
+ import java.util.Scanner;
 public class Myclass {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) { int num;
     System.out.println("Enter an Integer number:");
 
-    //The input provided by user is stored in num
+   
     Scanner input = new Scanner(System.in);
     num = input.nextInt();
 
-    /* If number is divisible by 2 then it's an even number
-     * else odd number*/
+  
     if ( num % 2 == 0 )
         System.out.println("Entered number is even");
      else
         System.out.println("Entered number is odd");
-        // TODO code application logic here
+        
     }
     
 }
